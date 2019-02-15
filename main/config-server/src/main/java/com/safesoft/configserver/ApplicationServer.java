9a56a68202c0990102ec4151d4e5292da721d002
eatyu <yzjworking@163.com>
2019-02-15 16:02:01 +0800

    配置项服务

--- main/config-server/src/main/java/com/safesoft/configserver/ApplicationServer.java
+++ main/config-server/src/main/java/com/safesoft/configserver/ApplicationServer.java
@@ -1,5 +1,21 @@
 package com.safesoft.configserver;
 
+/**
+ * SpringBoot的核心思想就是约定优于配置，所以仓库中的配置文件会被转换成web接口，访问可以参照以下的规则
+ *
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ *
+ * 访问http://localhost:8001/config-test.properties
+ * 符合第四条
+ *
+ *  application = config
+ *  profile = dev
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -13,3 +29,24 @@ public class ApplicationServer {
         SpringApplication.run(ApplicationServer.class,args);
     }
 }
+
+/**
+ * 访问下面 可以看到效果
+ * http://localhost:8001/config/dev
+ * http://localhost:8001/config-test.properties
+ *
+ *
+ * 我放置的3个文件是
+ * config-dev.properties
+ * config-pro.properties
+ * config-test.properties
+ *
+ * http://localhost:8001/config/dev 中的config，dev分别对应config-dev
+ *
+ * 如果是
+ * jerry-config-dev.properties
+ * 访问路径为
+ * http://localhost:8001/jerry-config/dev
+ *
+ *
+ */
