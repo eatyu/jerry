b9974dcdfe9161ab62eb42b728df34050279e0bc
eatyu <yzjworking@163.com>
2019-02-15 16:18:48 +0800

    客户端访问配置服务器配置信息

---
+++ main/config-server/src/main/java/com/safesoft/configserver/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.safesoft.configserver.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Value("${label}")
+    private String label;
+
+    @RequestMapping("/hello")
+    public String test() {
+        return label;
+    }
+}
