c8a150879a7efa00e007f9a17b7e438e2ad498f4
eatyu <yzjworking@163.com>
2019-02-15 16:23:37 +0800

    bug fixed

--- main/config-client/src/main/java/com.safesoft.configclient/controller/IndexController.java
+++ main/config-client/src/main/java/com.safesoft.configclient/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.safesoft.configserver.controller;
+package com.safesoft.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
