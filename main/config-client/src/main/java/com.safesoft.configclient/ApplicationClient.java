e8af25213dee98eec9fc72acb4360de45b199b9b
eatyu <yzjworking@163.com>
2019-02-18 10:23:14 +0800

    多个配置中心 注册到注册中心

--- main/config-client/src/main/java/com.safesoft.configclient/ApplicationClient.java
+++ main/config-client/src/main/java/com.safesoft.configclient/ApplicationClient.java
@@ -2,8 +2,10 @@ package com.safesoft.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApplicationClient {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationClient.class, args);
--- main/config-server/src/main/java/com/safesoft/configserver/ApplicationServer.java
+++ main/config-server/src/main/java/com/safesoft/configserver/ApplicationServer.java
@@ -19,11 +19,13 @@ package com.safesoft.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //使用@EnableConfigServer开启配置中心
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ApplicationServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationServer.class,args);
