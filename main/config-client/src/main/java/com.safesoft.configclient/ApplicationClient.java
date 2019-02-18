4dec1c8b14bb3d8db1d3a5a0690947cbb27b77ab
eatyu <yzjworking@163.com>
2019-02-18 15:33:31 +0800

    消息总线应用

--- main/config-client/src/main/java/com.safesoft.configclient/ApplicationClient.java
+++ main/config-client/src/main/java/com.safesoft.configclient/ApplicationClient.java
@@ -2,10 +2,12 @@ package com.safesoft.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ApplicationClient {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationClient.class, args);
