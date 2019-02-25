4909464fefe3bc40ac86ccc24c77bca9bca4456f
eatyu <yzjworking@163.com>
2019-02-25 16:05:36 +0800

    添加zipkin链路追踪

--- sleuthmain/zipkin/src/main/java/com/jerry/zipkin/ZipkinStart.java
+++ sleuthmain/zipkin/src/main/java/com/jerry/zipkin/ZipkinStart.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
-@EnableZipkinServer
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinStart {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinStart.class, args);
