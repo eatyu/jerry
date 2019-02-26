f5b652f424a3269925ca7ff726a8c5667e3e6994
eatyu <yzjworking@163.com>
2019-02-26 14:57:07 +0800

    更新sleuthmain工程 修复不能储存链路信息到mysql的bug 版本的问题，也有配置的问题。

--- sleuthmain/zipkin/src/main/java/com/jerry/zipkin/ZipkinStart.java
+++ sleuthmain/zipkin/src/main/java/com/jerry/zipkin/ZipkinStart.java
@@ -2,12 +2,23 @@ package com.jerry.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
 @EnableZipkinServer
+@SpringBootApplication
 public class ZipkinStart {
+
     public static void main(String[] args) {
         SpringApplication.run(ZipkinStart.class, args);
+    }
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
     }
 }
